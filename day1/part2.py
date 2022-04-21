1a89f0d08b99f185f1a3069fc36a18f493bddac4
Nuray9 <90618465+Nuray9@users.noreply.github.com>
2022-04-21 16:52:34 +0600

    Add files via upload

---
+++ day1/part2.py
@@ -0,0 +1,19 @@
+k = 0
+
+with open("input.txt") as f:
+    st = ''.join(f.readlines())
+
+
+for i in range(len(st)):
+    if k >= 0:
+        if st[i] == "(":
+            k += 1
+        else:
+            k -= 1
+    else:
+        k = i
+        break
+
+
+with open("output1.txt", "w") as f:
+    print(k, file=f)
